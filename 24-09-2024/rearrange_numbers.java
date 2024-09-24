d48fe7cdae3ecdc7f07045752c0d21f9696cd5d0
Varun-MSI <tvarun2014@gmail.com>
2024-09-24 13:37:23 +0530

    New

--- 24-09-2024/rearrange_numbers.java
+++ 24-09-2024/rearrange_numbers.java
@@ -34,6 +34,71 @@
 // -----------------
 // [-4, -4, -5, -6, 5, 3, 2, 1]
 
-public class rearrange_numbers {
-    
+import java.util.*;
+public class rearrange_numbers{
+    static public void arrange(int[] arr, int low, int high){
+        int i = 0, j = 0, k = low;
+        int mid = (low+high)/2;
+        int n1 = mid - low +1;
+        int n2 = high-mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        for(i = 0;i<n1;i++){
+            left[i] = arr[low+i];
+        }
+        for(j = 0;j<n2;j++){
+            right[j] = arr[mid+1+j];
+        }
+        i = 0;
+        j = 0;
+        while(i<n1){
+            if(left[i]<0){
+                arr[k] = left[i];
+                k++;
+            }
+            i++;
+        }
+        while(j<n2){
+            if(right[j]<0){
+                arr[k] = right[j];
+                k++;
+            }
+            j++;
+        }
+        i = 0;
+        j = 0;
+        while(i<n1){
+            if(left[i]>=0){
+                arr[k] = left[i];
+                k++;
+            }
+            i++;
+        }
+        while(j<n2){
+            if(right[j]>=0){
+                arr[k] = right[j];
+                k++;
+            }
+            j++;
+        }
+    }
+    static public void rearrange(int[] arr, int low, int high){
+        if(low<high){
+            int mid = (high+low)/2;
+            rearrange(arr, low, mid);
+            rearrange(arr, mid+1, high);
+            arrange(arr,low,high);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        rearrange(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+    }
 }
