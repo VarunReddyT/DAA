fce667092bea772d4496a6c8635eb43508ddb823
Varun-MSI <tvarun2014@gmail.com>
2024-09-24 12:38:59 +0530

    New

---
+++ 24-09-2024/pow.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class pow{
+    static public float power(int x, int y){
+        if(y == 0){
+            return 1;
+        }
+        float temp, res;
+        temp = power(x,y/2);
+        if(y%2 == 0){
+            res = temp*temp;
+        }
+        else{
+            if(y>0){
+                res = x*temp*temp;
+            }
+            else{
+                res = (temp*temp)/x;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int p = sc.nextInt();
+        System.out.println(power(n,p));
+        sc.close();
+    }
+}
