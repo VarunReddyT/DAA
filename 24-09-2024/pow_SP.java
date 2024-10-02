3a1c6fad80735d383a1326894b81815754105a70
Varun Reddy <tvarun2014@gmail.com>
2024-10-02 17:06:00 +0530

    New

--- 24-09-2024/pow_SP.java
+++ 24-09-2024/pow_SP.java
@@ -1,3 +1,39 @@
+// A school kid named John Doe, wants to find the value of N^P
+// Pth power of a number N.
+
+// You will be given two integers N and P.
+// Your task is to help John Doe to find the value of N^P.
+
+// NOTE:
+// ---------
+// Value of N and P can be both positive and negative
+
+// Input Format:
+// -------------
+// Line-1: Two integers N and P, where N is the number and P is the power
+
+// Output Format:
+// --------------
+// Line-1: Print a float number, N power P
+
+
+// Sample Input-1:
+// ---------------
+// 3 2
+
+// Sample Output-1:
+// ----------------
+// 9.000000
+
+
+// Sample Input-2:
+// ---------------
+// 2 -3
+
+// Sample Output-2:
+// ----------------
+// 0.125000
+
 import java.util.*;
 
 public class pow_SP {
