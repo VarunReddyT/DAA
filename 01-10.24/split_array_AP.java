fcdcd80845688847ecf781b6ecb7f48f4fb8cb7e
Varun Reddy <tvarun2014@gmail.com>
2024-10-02 10:44:00 +0530

    New

--- 01-10.24/split_array_AP.java
+++ 01-10.24/split_array_AP.java
@@ -43,6 +43,54 @@
 // Explanation:
 // The best way to split nums into two subarrays is [1, 2, 3] and [4, 5], where the largest sum is 9.
 
-public class split_array_AP {
-    
+import java.util.*;
+
+public class split_array_AP{
+    public static boolean splitArray(int[] arr, int mid, int k){
+        int count = 1;
+        int sum = 0;
+        for(int i : arr){
+            if(sum + i > mid){
+                count++;
+                sum = i;
+                if(count > k){
+                    return false;
+                }
+            }
+            else{
+                sum += i;
+            }
+        }
+        return true;
+    }
+    public static int binarySearch(int[] arr, int k){
+        int left = arr[0];
+        int right = 0;
+        for(int i : arr){
+            left = Math.max(left,i);
+            right += i; 
+        }
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(splitArray(arr,mid,k)){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] array = s.split(",");
+        int[] arr = new int[array.length];
+        for(int i = 0;i<array.length;i++){
+            arr[i] = Integer.parseInt(array[i]);
+        }
+        int k = sc.nextInt();
+        System.out.println(binarySearch(arr,k));
+        sc.close();
+    }
 }
