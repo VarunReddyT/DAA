c5c291bbf15b29acc63a2045857ee4fceed45936
VarunReddyT <tvarun2014@gmail.com>
2024-11-20 08:35:11 +0530

    New

--- 19-11-24 Backtracking/NQueenProblem.java
+++ 19-11-24 Backtracking/NQueenProblem.java
@@ -54,8 +54,47 @@ public class NQueenProblem {
     //Your utility functions goes here
 	boolean solveNQ(){
 	    //Write your code here and return boolean value
-		return false;
+		int[][] board = new int[N][N];
+		if(solveNQUtil(board, 0)==false){
+			System.out.println("No Solution");
+			return false;
+		}
+		printSolution(board);
+		return true;
 		
+	}
+	boolean solveNQUtil(int[][] board, int col){
+		if(col>=N){
+			return true;
+		}
+		for(int i = 0;i<board.length;i++){
+			if(isSafe(board,i,col)){
+				board[i][col] = 1;
+				if(solveNQUtil(board,col+1)==true){
+					return true;
+				}
+				board[i][col] = 0;
+			}
+		}
+		return false;
+	}
+	boolean isSafe(int[][] board, int row, int col){
+		for(int i = 0;i<col; i++){
+			if(board[row][i]==1){
+				return false;
+			}
+		}
+		for(int i = row, j = col; i>=0 && j>=0; i--,j--){
+			if(board[i][j]==1){
+				return false;
+			}
+		}
+		for(int i = row, j = col; i<board.length && j>=0; i++,j--){
+			if(board[i][j]==1){
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public static void main(String args[])
